d2a525e260eb249babe2fd6854b01dc7f00b00c3
Brian Lester <blester125@gmail.com>
2018-05-04 17:10:54 -0400

    New Setup

--- setup.py
+++ setup.py
@@ -1,20 +1,34 @@
 from setuptools import setup, find_packages
 
-version = "0.1.0"
-name = "quick_knn"
+class About(object):
+    NAME='quick_knn'
+    VERSION='0.1.1'
+    AUTHOR='blester125'
+    EMAIL=f'{AUTHOR}@gmail.com'
+    URL=f'https://github.com/{AUTHOR}/{NAME}'
+    DL_URL=f'{URL}/archive/{VERSION}.tar.gz'
+    LICENSE='MIT'
 
 setup(
-    name=name,
-    version=version,
-    description="LSH with MinHashing and Cosine Distance",
-    author="Brian Lester",
-    author_email="blester125@gmail.com",
-    url=f"https://github.com/blester125/{name}",
-    download_url=f"https://github.com/blester125/{name}/archive/{version}.tar.gz",
-    license="MIT",
+    name=About.NAME,
+    version=About.VERSION,
+    description='LSH for Jaccard and Cosine Similarity',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author=About.AUTHOR,
+    author_email=About.EMAIL,
+    url=About.URL,
+    download_url=About.DL_URL,
+    license=About.LICENSE,
     packages=find_packages(),
+    package_data={
+        'quick_knn': [
+        ],
+    },
+    include_package_data=True,
     install_requires=[
         'numpy',
     ],
-    keywords=['Data Mining']
+    keywords=['Data Mining'],
+    ext_modules=[],
 )
